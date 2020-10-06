8863ec5a6eb88e4fd1221460cef6612c625d62fd
Rodrigo Fernandes <f.rodrigo095@gmail.com>
2020-10-06 11:54:26 -0300

    Criando a logica do jogo da memoria

--- script.js
+++ script.js
@@ -1,9 +1,66 @@
 const cards = document.querySelectorAll('.card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard; 
+let lockBoard = false;
+
 function flipCard() {
-  this.classList.toggle('flip');
+
+  if(lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+  checkForMatch();
 }
+
+function checkForMatch() {
+  if(firstCard.dataset.card === secondCard.dataset.card) {
+    disableCaresrds();
+    return;
+  }
+
+  unflipCard();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCard() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPosition = Math.floor(Math.random() * 12);
+    card.style.order = randomPosition;
+  })
+})();
 
 cards.forEach((card) =>{
   card.addEventListener('click', flipCard);
-})
+});
+
